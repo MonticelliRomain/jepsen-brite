d197e9a667943c37161bd650cb096a3b9a8fd19e
marween <annemagali.saintgeorges@gmail.com>
2019-04-04 23:08:51 +0200

    connection db heroku

--- config/database.php
+++ config/database.php
@@ -1,10 +1,5 @@
 <?php
-$url = parse_url(getenv("DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 return [
 
     /*
@@ -64,16 +59,19 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => array(
-                'driver'   => 'pgsql',
-                'host'     => $host,
-                'database' => $database,
-                'username' => $username,
-                'password' => $password,
-                'charset'  => 'utf8',
-                'prefix'   => '',
-                'schema'   => 'public',
-    ),
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'brite'),
+            'username' => env('DB_USERNAME', 'becode'),
+            'password' => env('DB_PASSWORD', 'becode'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
