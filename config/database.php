dad4c86fc705a31c0c1f22716bc3d95c5921dc52
marween <annemagali.saintgeorges@gmail.com>
2019-04-04 19:12:47 +0200

    connection db heroku

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'bride'),
-            'username' => env('DB_USERNAME', 'becode'),
-            'password' => env('DB_PASSWORD', 'becode'),
+            'host' => env('DB_HOST', $host),
+            'port' => env('DB_PORT', $port),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
